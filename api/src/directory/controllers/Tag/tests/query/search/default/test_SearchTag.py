51663fd49d8ddbef5dadc2a74ba8e2091ad2f489
Jesse Rehrer <jesse.rehrer@l3harris.com>
2024-10-15 16:25:17 -0400

    PMBIPO-122: Implement controller tests.

--- api/src/directory/controllers/Tag/tests/query/search/default/test_SearchTag.py
+++ api/src/directory/controllers/Tag/tests/query/search/default/test_SearchTag.py
@@ -4,7 +4,12 @@ Collection of pytests for Tag's search controller.
 
 from typing import List
 
+from django.db.models import QuerySet
 from django.test import tag, TestCase
+
+from directory import controllers, models
+from directory.controllers.Tag.tests.query.search.default import arguments
+from directory.models.Tag.serializers import TagSerializer
 
 
 @tag(
@@ -26,3 +31,31 @@ class TestSearchTag(TestCase):
     def test_search_tags(self) -> None:
         """Success Case: Search for `Tag` records that match
         the given label."""
+
+        # Call search controller.
+        search_results: QuerySet[models.Tag] = controllers.Tag.search_tags(
+            arguments.SEARCH_TERM
+        )
+
+        # Ensure two `Tag`s are returned.
+        self.assertEqual(len(search_results), 2)
+
+        # Ensure each `Tag` label starts with the search term.
+        for result in search_results:
+            self.assertIsInstance(result, models.Tag)
+            self.assertTrue(result.label.startswith(arguments.SEARCH_TERM))
+            serialized_tag = TagSerializer(result).data
+            self.assertEqual(serialized_tag, arguments.EXPECTED_TAG_RECORDS[result.id])
+
+    @tag("controllers.tag.search_tags_no_results")
+    def test_search_tags_no_results(self) -> None:
+        """Success Case: Search for `Tag` records that match
+        the given label but none exist."""
+
+        # Call search controller.
+        search_results: QuerySet[models.Tag] = controllers.Tag.search_tags(
+            arguments.SEARCH_TERM_NO_RESULTS
+        )
+
+        # Ensure an empty QuerySet is returned.
+        self.assertEqual(len(search_results), 0)
