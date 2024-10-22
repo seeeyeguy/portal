d20f3a07235477f72ec3cccbab368658458de250
Cantrell Picou <Cantrell.Picou@l3harris.com>
2024-10-22 12:43:27 -0400

    PMBIPO-119: Implemented Function Controller Test.

--- api/src/directory/controllers/Function/tests/query/read/default/arguments/__init__.py
+++ api/src/directory/controllers/Function/tests/query/read/default/arguments/__init__.py
@@ -2,3 +2,49 @@
 Arguments to be shared for Function's fetch
 controller pytests.
 """
+
+# Valid `Function` ids.
+FUNCTION_ID_1: int = 1
+FUNCTION_ID_2: int = 2
+FUNCTION_ID_3: int = 3
+FUNCTION_ID_4: int = 4
+
+# Id used for testing fetch by function id
+# tests.
+FETCH_FUNCTION_BY_ID: int = 4
+
+# Id used for testing fetch by function id
+# DNE tests.
+FETCH_FUNCTION_BY_ID_DNE: int = 99
+
+# Valid `Function` records dictionary.
+VALID_FUNCTION_RECORDS: dict = {
+    FUNCTION_ID_1: {
+        "id": 1,
+        "name": "Human Resources",
+        "description": "Function 1.",
+        "created": "2024-08-27T12:00:00-04:00",
+        "modified": "2024-08-27T12:00:00-04:00",
+    },
+    FUNCTION_ID_2: {
+        "id": 2,
+        "name": "Engineering",
+        "description": "Function 2.",
+        "created": "2024-08-27T12:00:00-04:00",
+        "modified": "2024-08-27T12:00:00-04:00",
+    },
+    FUNCTION_ID_3: {
+        "id": 3,
+        "name": "Finance",
+        "description": "Function 3.",
+        "created": "2024-08-27T12:00:00-04:00",
+        "modified": "2024-08-27T12:00:00-04:00",
+    },
+    FUNCTION_ID_4: {
+        "id": 4,
+        "name": "Operations",
+        "description": "Function 4.",
+        "created": "2024-08-27T12:00:00-04:00",
+        "modified": "2024-08-27T12:00:00-04:00",
+    },
+}
--- api/src/directory/controllers/Function/tests/query/read/default/test_FetchFunction.py
+++ api/src/directory/controllers/Function/tests/query/read/default/test_FetchFunction.py
@@ -2,9 +2,19 @@
 Collection of pytests for Function's fetch controller.
 """
 
-from typing import List
+import pytest
+from typing import List, Union
 
+from django.db.models import QuerySet
 from django.test import tag, TestCase
+
+from directory.controllers.Function.Function import Function
+from directory.controllers.Function.tests.query.read.default import arguments
+from directory.exceptions import DirectoryError
+from directory.models.Function.Function import (
+    Function as FunctionModel,
+)
+from directory.models.Function.serializers import FunctionSerializer
 
 
 @tag(
@@ -26,11 +36,42 @@ class TestFetchFunction(TestCase):
     def test_fetch_functions(self) -> None:
         """Success Case: Fetch all `Function` records."""
 
+        functions = Function.fetch_functions()
+
+        self.assertIsInstance(functions, QuerySet[FunctionModel])
+        self.assertEqual(
+            functions.count(), len(arguments.VALID_FUNCTION_RECORDS.keys())  # type: ignore[union-attr]
+        )
+
+        for function in functions:  # type: ignore[union-attr]
+            self.assertIsInstance(function, FunctionModel)
+            function_id: int = function.id
+            self.assertIn(function_id, arguments.VALID_FUNCTION_RECORDS)
+            self.assertEqual(
+                FunctionSerializer(function).data,
+                arguments.VALID_FUNCTION_RECORDS[function_id],
+            )
+
     @tag("controllers.function.fetch_function_by_id")
     def test_fetch_function_by_id(self) -> None:
         """Success Case: Fetch a `Function` record given an id."""
+
+        function: Union[
+            FunctionModel, QuerySet[FunctionModel]
+        ] = Function.fetch_functions(function_id=arguments.FETCH_FUNCTION_BY_ID)
+
+        self.assertIsInstance(function, FunctionModel)
+        function_id: int = function.id  # type: ignore[union-attr]
+        self.assertIn(function_id, arguments.VALID_FUNCTION_RECORDS)
+        self.assertEqual(
+            FunctionSerializer(function).data,
+            arguments.VALID_FUNCTION_RECORDS[function_id],
+        )
 
     @tag("controllers.function.fetch_function_by_id_dne")
     def test_fetch_function_by_id_dne(self) -> None:
         """Fail Case: Fetch a `Function` record given an id where
         record does not exist."""
+
+        with pytest.raises(DirectoryError):
+            _ = Function.fetch_functions(function_id=arguments.FETCH_FUNCTION_BY_ID_DNE)
