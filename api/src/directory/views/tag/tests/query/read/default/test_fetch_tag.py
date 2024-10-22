a9f145e91b5f8b44c34bc3de5b7f0c037645a08c
Luis Ruelas Lisboa <Luis.RuelasLisboa@L3Harris.com>
2024-10-22 17:06:06 -0400

    PMBIPO-121: Implemented GET tags view pytests and documented endpoint.

--- api/src/directory/views/tag/tests/query/read/default/test_fetch_tag.py
+++ api/src/directory/views/tag/tests/query/read/default/test_fetch_tag.py
@@ -2,10 +2,14 @@
 Collection of pytests for Tag's fetch view endpoint.
 """
 
+# pylint: disable=line-too-long
 from typing import List
 
 from django.test import tag, TestCase
 from django.urls import reverse
+from rest_framework import status
+
+from directory.controllers.Tag.tests.query.read.default import arguments
 
 
 @tag(
@@ -31,23 +35,158 @@ class TestFetchTag(TestCase):
     def test_fetch_tags(self) -> None:
         """Success Case: Fetch all `Tag` records."""
 
+        response = self.client.get(self.url)
+
+        tags = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(tags, List)
+        self.assertEqual(len(tags), len(arguments.VALID_TAG_RECORDS))
+
+        for tag in tags:
+            self.assertIsInstance(tag, dict)
+
+            tag_id: int = tag["id"]
+
+            self.assertIn(tag_id, arguments.VALID_TAG_RECORDS)
+            self.assertEqual(tag, arguments.VALID_TAG_RECORDS[tag_id])
+
     @tag("views.tag.fetch_tags_with_page")
     def test_fetch_tags_with_page(self) -> None:
         """Success Case: Fetch page of `Tag` records."""
+
+        request_url: str = f"{self.url}?page={arguments.FETCH_TAG_WITH_PAGE}"
+        response = self.client.get(request_url)
+
+        tags = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(tags, List)
+        self.assertEqual(len(tags), len(arguments.VALID_TAG_RECORDS))
+
+        for tag in tags:
+            self.assertIsInstance(tag, dict)
+
+            tag_id: int = tag["id"]
+
+            self.assertIn(tag_id, arguments.VALID_TAG_RECORDS)
+            self.assertEqual(tag, arguments.VALID_TAG_RECORDS[tag_id])
 
     @tag("views.tag.fetch_tags_with_limit")
     def test_fetch_tags_with_limit(self) -> None:
         """Success Case: Fetch all `Tag` records up to limit."""
 
+        request_url: str = f"{self.url}?limit={arguments.FETCH_TAG_WITH_LIMIT}"
+        response = self.client.get(request_url)
+
+        tags = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(tags, List)
+        self.assertEqual(len(tags), len(arguments.FETCH_TAG_WITH_LIMIT_VALID_IDS))
+
+        for tag in tags:
+            self.assertIsInstance(tag, dict)
+
+            tag_id: int = tag["id"]
+
+            self.assertIn(tag_id, arguments.FETCH_TAG_WITH_LIMIT_VALID_IDS)
+            self.assertEqual(tag, arguments.VALID_TAG_RECORDS[tag_id])
+
     @tag("views.tag.fetch_tags_with_page_and_limit")
     def test_fetch_tags_with_page_and_limit(self) -> None:
         """Success Case: Fetch page of `Tag` records up to limit."""
+
+        request_url: str = f"{self.url}?page={arguments.FETCH_TAG_WITH_PAGE}&limit={arguments.FETCH_TAG_WITH_PAGE_AND_LIMIT_LIMIT_NUMBER}"
+        response = self.client.get(request_url)
+
+        tags = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(tags, List)
+        self.assertEqual(
+            len(tags), len(arguments.FETCH_TAG_WITH_PAGE_AND_LIMIT_VALID_IDS)
+        )
+
+        for tag in tags:
+            self.assertIsInstance(tag, dict)
+
+            tag_id: int = tag["id"]
+
+            self.assertIn(tag_id, arguments.FETCH_TAG_WITH_PAGE_AND_LIMIT_VALID_IDS)
+            self.assertEqual(tag, arguments.VALID_TAG_RECORDS[tag_id])
+
+    @tag("views.tag.fetch_tags_with_page_exceeding_page_count")
+    def test_fetch_tags_with_page_exceeding_page_count(self) -> None:
+        """Success Case: Fetch page of `Tag` records using a
+        page number that exceeds the number of pages."""
+
+        request_url: str = (
+            f"{self.url}?page={arguments.FETCH_TAG_WITH_PAGE_EXCEEDING_PAGE_COUNT}"
+        )
+        response = self.client.get(request_url)
+
+        tags = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(tags, List)
+        self.assertEqual(
+            len(tags), arguments.FETCH_TAG_WITH_PAGE_EXCEEDING_PAGE_COUNT_RECORD_COUNT
+        )
 
     @tag("views.tag.fetch_tag_by_id")
     def test_fetch_tag_by_id(self) -> None:
         """Success Case: Fetch a `Tag` record given an id."""
 
+        request_url: str = f"{self.url}?id={arguments.FETCH_TAG_BY_ID}"
+        response = self.client.get(request_url)
+
+        tag = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(tag, dict)
+
+        tag_id: int = tag["id"]
+
+        self.assertIn(tag_id, arguments.VALID_TAG_RECORDS)
+        self.assertEqual(tag, arguments.VALID_TAG_RECORDS[tag_id])
+
     @tag("views.tag.fetch_tag_by_id_dne")
     def test_fetch_tag_by_id_dne(self) -> None:
         """Fail Case: Fetch a `Tag` record given an id where
         record does not exist."""
+
+        request_url: str = f"{self.url}?id={arguments.FETCH_TAG_BY_ID_DNE}"
+        response = self.client.get(request_url)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    @tag("views.tag.fetch_tag_by_id_with_page")
+    def test_fetch_tag_by_id_with_page(self) -> None:
+        """Fail Case: Fetch a `Tag` record given an id where
+        a page is also supplied in the parameters."""
+
+        request_url: str = f"{self.url}?id={arguments.FETCH_TAG_BY_ID_DNE}&page={arguments.FETCH_TAG_WITH_PAGE}"
+        response = self.client.get(request_url)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    @tag("views.tag.fetch_tag_by_id_with_limit")
+    def test_fetch_tag_by_id_with_limit(self) -> None:
+        """Fail Case: Fetch a `Tag` record given an id where
+        a limit is also supplied in the parameters."""
+
+        request_url: str = f"{self.url}?id={arguments.FETCH_TAG_BY_ID_DNE}&limit={arguments.FETCH_TAG_WITH_LIMIT}"
+        response = self.client.get(request_url)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    @tag("views.tag.fetch_tag_by_id_with_page_and_limit")
+    def test_fetch_tag_by_id_with_page_and_limit(self) -> None:
+        """Fail Case: Fetch a `Tag` record given an id where
+        a page and limit is also supplied in the parameters."""
+
+        request_url: str = f"{self.url}?id={arguments.FETCH_TAG_BY_ID_DNE}&page={arguments.FETCH_TAG_WITH_PAGE}&limit={arguments.FETCH_TAG_WITH_LIMIT}"
+        response = self.client.get(request_url)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
