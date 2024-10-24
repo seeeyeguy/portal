785e5b401cfc99631cef85d74a54246be3b12963
Luis Ruelas Lisboa <Luis.RuelasLisboa@L3Harris.com>
2024-10-24 09:11:28 -0400

    PMBIPO-128: Implemented PUT queryfilerstate controller tests and added test fixtures.

--- api/src/preferences/controllers/QueryFilterState/tests/mutations/update/default/arguments/__init__.py
+++ api/src/preferences/controllers/QueryFilterState/tests/mutations/update/default/arguments/__init__.py
@@ -2,3 +2,139 @@
 Arguments to be shared for QueryFilterState's update
 controller pytests.
 """
+
+# pylint: disable=line-too-long
+from typing import List
+
+
+# `QueryFilterState` id used for testing
+# update.
+UPDATE_QUERYFILTERSTATE_ID: int = 1
+
+# `User` email used for testing update.
+UPDATE_QUERYFILTERSTATE_USER_EMAIL: str = "May.Parker@harris.com"
+
+# `Query` id used for testing update.
+UPDATE_QUERYFILTERSTATE_QUERY_ID: int = 1
+
+# `Function` ids used for updating a `QueryFilterState`
+# record.
+UPDATE_QUERYFILTERSTATE_FUNCTION_IDS: List[int] = [3, 4]
+
+# `EmployeeLevel` ids used for updating a `QueryFilterState`
+# record.
+UPDATE_QUERYFILTERSTATE_EMPLOYEELEVEL_IDS: List[int] = [1, 2]
+
+# `Tag` ids used for updating a `QueryFilterState`
+# record.
+UPDATE_QUERYFILTERSTATE_TAG_IDS: List[int] = [5, 6]
+
+# `QueryFilterState` id that does not exist.
+UPDATE_QUERYFILTERSTATE_ID_DNE: int = 20
+
+# `User` email that does not exist.
+UPDATE_QUERYFILTERSTATE_USER_EMAIL_DNE: str = "Test.UserDNE@harris.com"
+
+# `User` email that does not match the
+#  existing `QueryFilterState` record's user email.
+UPDATE_QUERYFILTERSTATE_USER_EMAIL_DOES_NOT_MATCH: str = "Peter.Parker@harris.com"
+
+# `Query` id that does not exist.
+UPDATE_QUERYFILTERSTATE_QUERY_ID_DNE: int = 30
+
+# `Function` ids that do not exist.
+UPDATE_QUERYFILTERSTATE_FUNCTION_IDS_DNE: List[int] = [40, 50]
+
+# `EmployeeLevel` ids that do not exist.
+UPDATE_QUERYFILTERSTATE_EMPLOYEELEVEL_IDS_DNE: List[int] = [60, 70]
+
+# `Tag` ids that do not exist.
+UPDATE_QUERYFILTERSTATE_TAG_IDS_DNE: List[int] = [80, 90]
+
+# Valid `QueryFilterState` record.
+VALID_QUERYFILTERSTATE_RECORD: dict = {
+    "id": 1,
+    "search": {
+        "id": 1,
+        "user": {
+            "id": 1,
+            "username": "May.Parker@harris.com",
+            "email": "May.Parker@harris.com",
+            "first_name": "May",
+            "last_name": "Parker",
+            "is_active": True,
+        },
+        "resources": [
+            {
+                "id": 1,
+                "employee_levels": [
+                    {
+                        "id": 1,
+                        "name": "Employee",
+                        "description": "Employee Level 1.",
+                        "created": "2024-08-27T12:00:00-04:00",
+                        "modified": "2024-08-27T12:00:00-04:00",
+                        "level": 1,
+                    },
+                    {
+                        "id": 3,
+                        "name": "Executive",
+                        "description": "Executive Level 1.",
+                        "created": "2024-08-27T12:00:00-04:00",
+                        "modified": "2024-08-27T12:00:00-04:00",
+                        "level": 3,
+                    },
+                ],
+                "subfunctions": [
+                    {
+                        "id": 1,
+                        "function": {
+                            "id": 1,
+                            "name": "Human Resources",
+                            "description": "Function 1.",
+                            "created": "2024-08-27T12:00:00-04:00",
+                            "modified": "2024-08-27T12:00:00-04:00",
+                        },
+                        "name": "Training",
+                        "description": "Function 1 SubFunction 1.",
+                        "created": "2024-08-27T12:00:00-04:00",
+                        "modified": "2024-08-27T12:00:00-04:00",
+                    }
+                ],
+                "tags": [
+                    {
+                        "id": 5,
+                        "created": "2024-08-27T12:00:00-04:00",
+                        "modified": "2024-08-27T12:00:00-04:00",
+                        "label": "Engineering Tag",
+                    }
+                ],
+                "description": "A cinema resource.",
+                "created": "2024-10-01T12:00:00-04:00",
+                "uid": "d2ab62e8-4a0e-4b12-b94b-c128cae48222",
+                "revision_number": 1,
+                "name": "Resource 1 Revision 1",
+                "url": "example-revision-site.org",
+                "thumbnail": "/v1/media/resources/thumbnails/d2ab62e8-4a0e-4b12-b94b-c128cae48222/1/resource_1.png",
+                "type": "Resource Type 1",
+                "download": False,
+                "active": True,
+                "previous_revision": None,
+            }
+        ],
+        "created": "2024-10-01T12:00:00-04:00",
+        "search_term": "cinema",
+    },
+    "user": {
+        "id": 1,
+        "username": "May.Parker@harris.com",
+        "email": "May.Parker@harris.com",
+        "first_name": "May",
+        "last_name": "Parker",
+        "is_active": True,
+    },
+    "functions": [3, 4],
+    "employee_levels": [1, 2],
+    "tags": [5, 6],
+    "created": "2024-10-01T12:00:00-04:00",
+}
--- api/src/preferences/controllers/QueryFilterState/tests/mutations/update/default/test_UpdateQueryFilterState.py
+++ api/src/preferences/controllers/QueryFilterState/tests/mutations/update/default/test_UpdateQueryFilterState.py
@@ -2,11 +2,22 @@
 Collection of pytests for QueryFilterState's update controller.
 """
 
+# pylint: disable=line-too-long,wrong-import-order
+import pytest
 from typing import List
 
 from django.test import tag, TestCase
 
 from portal.models.fixtures import COMMON_FIXTURES
+from preferences.controllers.QueryFilterState.QueryFilterState import QueryFilterState
+from preferences.controllers.QueryFilterState.tests.mutations.update.default import (
+    arguments,
+)
+from preferences.exceptions import PreferencesError
+from preferences.models.QueryFilterState.QueryFilterState import (
+    QueryFilterState as QueryFilterStateModel,
+)
+from preferences.models.QueryFilterState.serializers import QueryFilterStateSerializer
 
 
 @tag(
@@ -20,32 +31,144 @@ from portal.models.fixtures import COMMON_FIXTURES
 class TestUpdateQueryFilterState(TestCase):
     """Test suite for QueryFilterState's update controller."""
 
-    fixtures: List[str] = [*COMMON_FIXTURES]
+    fixtures: List[str] = [
+        *COMMON_FIXTURES,
+        "preferences/controllers/QueryFilterState/tests/mutations/update/default/fixtures/resources.json",
+        "preferences/controllers/QueryFilterState/tests/mutations/update/default/fixtures/requests.json",
+        "preferences/controllers/QueryFilterState/tests/mutations/update/default/fixtures/transitions.json",
+        "preferences/controllers/QueryFilterState/tests/mutations/update/default/fixtures/dispositions.json",
+        "preferences/controllers/QueryFilterState/tests/mutations/update/default/fixtures/queries.json",
+        "preferences/controllers/QueryFilterState/tests/mutations/update/default/fixtures/queryfilterstates.json",
+    ]
 
     @tag("controllers.queryfilterstate.update_query_filter_state")
     def test_update_query_filter_state(self) -> None:
         """Success Case: Update a `QueryFilterState` record."""
 
+        query_filter_state = QueryFilterState.update_query_filter_state(
+            record_id=arguments.UPDATE_QUERYFILTERSTATE_ID,
+            search=arguments.UPDATE_QUERYFILTERSTATE_QUERY_ID,
+            functions=arguments.UPDATE_QUERYFILTERSTATE_FUNCTION_IDS,
+            employee_levels=arguments.UPDATE_QUERYFILTERSTATE_EMPLOYEELEVEL_IDS,
+            tags=arguments.UPDATE_QUERYFILTERSTATE_TAG_IDS,
+            user=arguments.UPDATE_QUERYFILTERSTATE_USER_EMAIL,
+        )
+
+        self.assertIsInstance(query_filter_state, QueryFilterStateModel)
+
+        # Serialize `QueryFilterState`.
+        serialized_query_filter_state: dict = QueryFilterStateSerializer(
+            query_filter_state
+        ).data
+
+        # Remove dynamic datetime field before
+        # comparison.
+        del serialized_query_filter_state["modified"]
+
+        self.assertEqual(
+            serialized_query_filter_state, arguments.VALID_QUERYFILTERSTATE_RECORD
+        )
+
     @tag("controllers.queryfilterstate.update_query_filter_state_record_dne")
     def test_update_query_filter_state_record_dne(self) -> None:
         """Fail Case: Update a `QueryFilterState` that does not exist."""
+
+        with pytest.raises(PreferencesError):
+            _ = QueryFilterState.update_query_filter_state(
+                record_id=arguments.UPDATE_QUERYFILTERSTATE_ID_DNE,
+                search=arguments.UPDATE_QUERYFILTERSTATE_QUERY_ID,
+                functions=arguments.UPDATE_QUERYFILTERSTATE_FUNCTION_IDS,
+                employee_levels=arguments.UPDATE_QUERYFILTERSTATE_EMPLOYEELEVEL_IDS,
+                tags=arguments.UPDATE_QUERYFILTERSTATE_TAG_IDS,
+                user=arguments.UPDATE_QUERYFILTERSTATE_USER_EMAIL,
+            )
 
     @tag("controllers.queryfilterstate.update_query_filter_state_search_dne")
     def test_update_query_filter_state_search_dne(self) -> None:
         """Fail Case: Update a `QueryFilterState` record with a search `Query`
         that does not exist."""
 
+        with pytest.raises(PreferencesError):
+            _ = QueryFilterState.update_query_filter_state(
+                record_id=arguments.UPDATE_QUERYFILTERSTATE_ID,
+                search=arguments.UPDATE_QUERYFILTERSTATE_QUERY_ID_DNE,
+                functions=arguments.UPDATE_QUERYFILTERSTATE_FUNCTION_IDS,
+                employee_levels=arguments.UPDATE_QUERYFILTERSTATE_EMPLOYEELEVEL_IDS,
+                tags=arguments.UPDATE_QUERYFILTERSTATE_TAG_IDS,
+                user=arguments.UPDATE_QUERYFILTERSTATE_USER_EMAIL,
+            )
+
     @tag("controllers.queryfilterstate.update_query_filter_state_function_dne")
     def test_update_query_filter_state_function_dne(self) -> None:
         """Fail Case: Update a `QueryFilterState` record with a `Function`
         that does not exist."""
+
+        with pytest.raises(PreferencesError):
+            _ = QueryFilterState.update_query_filter_state(
+                record_id=arguments.UPDATE_QUERYFILTERSTATE_ID,
+                search=arguments.UPDATE_QUERYFILTERSTATE_QUERY_ID,
+                functions=arguments.UPDATE_QUERYFILTERSTATE_FUNCTION_IDS_DNE,
+                employee_levels=arguments.UPDATE_QUERYFILTERSTATE_EMPLOYEELEVEL_IDS,
+                tags=arguments.UPDATE_QUERYFILTERSTATE_TAG_IDS,
+                user=arguments.UPDATE_QUERYFILTERSTATE_USER_EMAIL,
+            )
 
     @tag("controllers.queryfilterstate.update_query_filter_state_employee_level_dne")
     def test_update_query_filter_state_employee_level_dne(self) -> None:
         """Fail Case: Update a `QueryFilterState` record with an `EmployeeLevel`
         that does not exist."""
 
+        with pytest.raises(PreferencesError):
+            _ = QueryFilterState.update_query_filter_state(
+                record_id=arguments.UPDATE_QUERYFILTERSTATE_ID,
+                search=arguments.UPDATE_QUERYFILTERSTATE_QUERY_ID,
+                functions=arguments.UPDATE_QUERYFILTERSTATE_FUNCTION_IDS,
+                employee_levels=arguments.UPDATE_QUERYFILTERSTATE_EMPLOYEELEVEL_IDS_DNE,
+                tags=arguments.UPDATE_QUERYFILTERSTATE_TAG_IDS,
+                user=arguments.UPDATE_QUERYFILTERSTATE_USER_EMAIL,
+            )
+
     @tag("controllers.queryfilterstate.update_query_filter_state_tag_dne")
     def test_update_query_filter_state_tag_dne(self) -> None:
         """Fail Case: Update a `QueryFilterState` record with a `Tag`
         that does not exist."""
+
+        with pytest.raises(PreferencesError):
+            _ = QueryFilterState.update_query_filter_state(
+                record_id=arguments.UPDATE_QUERYFILTERSTATE_ID,
+                search=arguments.UPDATE_QUERYFILTERSTATE_QUERY_ID,
+                functions=arguments.UPDATE_QUERYFILTERSTATE_FUNCTION_IDS,
+                employee_levels=arguments.UPDATE_QUERYFILTERSTATE_EMPLOYEELEVEL_IDS,
+                tags=arguments.UPDATE_QUERYFILTERSTATE_TAG_IDS_DNE,
+                user=arguments.UPDATE_QUERYFILTERSTATE_USER_EMAIL,
+            )
+
+    @tag("controllers.queryfilterstate.update_query_filter_state_user_dne")
+    def test_update_query_filter_state_user_dne(self) -> None:
+        """Fail Case: Update a `QueryFilterState` record using the
+        email of a `User` that does not exist."""
+
+        with pytest.raises(PreferencesError):
+            _ = QueryFilterState.update_query_filter_state(
+                record_id=arguments.UPDATE_QUERYFILTERSTATE_ID,
+                search=arguments.UPDATE_QUERYFILTERSTATE_QUERY_ID,
+                functions=arguments.UPDATE_QUERYFILTERSTATE_FUNCTION_IDS,
+                employee_levels=arguments.UPDATE_QUERYFILTERSTATE_EMPLOYEELEVEL_IDS,
+                tags=arguments.UPDATE_QUERYFILTERSTATE_TAG_IDS,
+                user=arguments.UPDATE_QUERYFILTERSTATE_USER_EMAIL_DNE,
+            )
+
+    @tag("controllers.queryfilterstate.update_query_filter_state_user_does_not_match")
+    def test_update_query_filter_state_user_does_not_match(self) -> None:
+        """Fail Case: Update a `QueryFilterState` record using the
+        email of a `User` that does not match the record's user email."""
+
+        with pytest.raises(PreferencesError):
+            _ = QueryFilterState.update_query_filter_state(
+                record_id=arguments.UPDATE_QUERYFILTERSTATE_ID,
+                search=arguments.UPDATE_QUERYFILTERSTATE_QUERY_ID,
+                functions=arguments.UPDATE_QUERYFILTERSTATE_FUNCTION_IDS,
+                employee_levels=arguments.UPDATE_QUERYFILTERSTATE_EMPLOYEELEVEL_IDS,
+                tags=arguments.UPDATE_QUERYFILTERSTATE_TAG_IDS,
+                user=arguments.UPDATE_QUERYFILTERSTATE_USER_EMAIL_DOES_NOT_MATCH,
+            )
