2e1e5c34153bd6761952edf91f6bfcbbce3be3c5
Robbins, Zemar (US) - SAS <zemar.robbins@l3harris.com>
2024-10-24 14:45:26 -0400

    PMBIPO-117: Implemented POST query controller pytests and added test fixtures.

--- api/src/analytics/controllers/Query/tests/mutations/create/default/arguments/__init__.py
+++ api/src/analytics/controllers/Query/tests/mutations/create/default/arguments/__init__.py
@@ -2,3 +2,156 @@
 Arguments to be shared for Query's create
 controller pytests.
 """
+
+# `User` email used for testing create `Query`.
+CREATE_QUERY_USER_EMAIL: str = "May.Parker@harris.com"
+
+# `User` DNE email used for testing failure case.
+CREATE_QUERY_USER_EMAIL_DNE: str = "non.existing.user@harris.com"
+
+# Search term used for testing create `Query`.
+CREATE_QUERY_SEARCH_TERM: str = "Resource"
+
+# Search term that yields no results.
+SEARCH_TERM_NO_RESULTS: str = "non_existent_term"
+
+# Valid `Query` to validate creation.
+VALID_CREATED_QUERY: dict = {
+    "user": {
+        "id": 1,
+        "username": "May.Parker@harris.com",
+        "email": "May.Parker@harris.com",
+        "first_name": "May",
+        "last_name": "Parker",
+        "is_active": True,
+    },
+    "resources": [
+        {
+            "id": 1,
+            "employee_levels": [
+                {
+                    "id": 1,
+                    "name": "Employee",
+                    "description": "Employee Level 1.",
+                    "created": "2024-08-27T12:00:00-04:00",
+                    "modified": "2024-08-27T12:00:00-04:00",
+                    "level": 1,
+                },
+                {
+                    "id": 2,
+                    "name": "Manager",
+                    "description": "Manager Level 1.",
+                    "created": "2024-08-27T12:00:00-04:00",
+                    "modified": "2024-08-27T12:00:00-04:00",
+                    "level": 2,
+                },
+            ],
+            "subfunctions": [
+                {
+                    "id": 1,
+                    "function": {
+                        "id": 1,
+                        "name": "Human Resources",
+                        "description": "Function 1.",
+                        "created": "2024-08-27T12:00:00-04:00",
+                        "modified": "2024-08-27T12:00:00-04:00",
+                    },
+                    "name": "Training",
+                    "description": "Function 1 SubFunction 1.",
+                    "created": "2024-08-27T12:00:00-04:00",
+                    "modified": "2024-08-27T12:00:00-04:00",
+                }
+            ],
+            "tags": [
+                {
+                    "id": 1,
+                    "created": "2024-08-27T12:00:00-04:00",
+                    "modified": "2024-08-27T12:00:00-04:00",
+                    "label": "filter::site:Melbourne",
+                }
+            ],
+            "description": "popstick.",
+            "created": "2024-08-27T12:00:00-04:00",
+            "uid": "f6a31db2-5271-401a-901e-9220e0e4cda2",
+            "revision_number": 1,
+            "name": "Resource 1 Revision 1",
+            "url": "example.org",
+            "thumbnail": "/v1/media/resources/thumbnails/f6a31db2-5271-401a-901e-9220e0e4cda2/1/resource_1.png",
+            "type": "Resource Type 1",
+            "download": False,
+            "active": True,
+            "previous_revision": None,
+        },
+        {
+            "id": 2,
+            "employee_levels": [
+                {
+                    "id": 1,
+                    "name": "Employee",
+                    "description": "Employee Level 1.",
+                    "created": "2024-08-27T12:00:00-04:00",
+                    "modified": "2024-08-27T12:00:00-04:00",
+                    "level": 1,
+                },
+                {
+                    "id": 2,
+                    "name": "Manager",
+                    "description": "Manager Level 1.",
+                    "created": "2024-08-27T12:00:00-04:00",
+                    "modified": "2024-08-27T12:00:00-04:00",
+                    "level": 2,
+                },
+            ],
+            "subfunctions": [
+                {
+                    "id": 1,
+                    "function": {
+                        "id": 1,
+                        "name": "Human Resources",
+                        "description": "Function 1.",
+                        "created": "2024-08-27T12:00:00-04:00",
+                        "modified": "2024-08-27T12:00:00-04:00",
+                    },
+                    "name": "Training",
+                    "description": "Function 1 SubFunction 1.",
+                    "created": "2024-08-27T12:00:00-04:00",
+                    "modified": "2024-08-27T12:00:00-04:00",
+                }
+            ],
+            "tags": [
+                {
+                    "id": 1,
+                    "created": "2024-08-27T12:00:00-04:00",
+                    "modified": "2024-08-27T12:00:00-04:00",
+                    "label": "filter::site:Melbourne",
+                }
+            ],
+            "description": "ripstick.",
+            "created": "2024-08-27T12:00:00-04:00",
+            "uid": "2ca5ca29-5dbb-4333-99d3-750e30b1ed48",
+            "revision_number": 1,
+            "name": "Resource 2 Revision 1",
+            "url": "example.gov",
+            "thumbnail": "/v1/media/resources/thumbnails/2ca5ca29-5dbb-4333-99d3-750e30b1ed48/2/resource_2.png",
+            "type": "Resource Type 2",
+            "download": False,
+            "active": True,
+            "previous_revision": None,
+        },
+    ],
+    "search_term": "Resource",
+}
+
+# Valid `Query` with no results.
+VALID_NO_RESULTS_QUERY: dict = {
+    "user": {
+        "id": 1,
+        "username": "May.Parker@harris.com",
+        "email": "May.Parker@harris.com",
+        "first_name": "May",
+        "last_name": "Parker",
+        "is_active": True,
+    },
+    "resources": [],
+    "search_term": "non_existent_term",
+}
--- api/src/analytics/controllers/Query/tests/mutations/create/default/test_CreateQuery.py
+++ api/src/analytics/controllers/Query/tests/mutations/create/default/test_CreateQuery.py
@@ -2,9 +2,17 @@
 Collection of pytests for Query's create controller.
 """
 
+# pylint: disable=wrong-import-order
+import pytest
 from typing import List
 
 from django.test import tag, TestCase
+
+from analytics.controllers.Query.Query import Query as QueryController
+from analytics.controllers.Query.tests.mutations.create.default import arguments
+from analytics.exceptions import AnalyticsError
+from analytics.models.Query.Query import Query as QueryModel
+from analytics.models.Query.serializers import QuerySerializer
 
 from portal.models.fixtures import COMMON_FIXTURES
 
@@ -20,18 +28,56 @@ from portal.models.fixtures import COMMON_FIXTURES
 class TestCreateQuery(TestCase):
     """Test suite for Query's create controller."""
 
-    fixtures: List[str] = [*COMMON_FIXTURES]
+    fixtures: List[str] = [
+        *COMMON_FIXTURES,
+        "analytics/controllers/Query/tests/mutations/create/default/fixtures/resources.json",
+        "analytics/controllers/Query/tests/mutations/create/default/fixtures/requests.json",
+        "analytics/controllers/Query/tests/mutations/create/default/fixtures/transitions.json",
+        "analytics/controllers/Query/tests/mutations/create/default/fixtures/dispositions.json",
+    ]
 
     @tag("controllers.query.create_query")
     def test_create_query(self) -> None:
         """Success Case: Create a `Query` record."""
 
+        query = QueryController.create_query(
+            user=arguments.CREATE_QUERY_USER_EMAIL,
+            search_term=arguments.CREATE_QUERY_SEARCH_TERM,
+        )
+
+        self.assertIsInstance(query, QueryModel)
+
+        serialized_query = QuerySerializer(query).data
+        del serialized_query["created"]
+        del serialized_query["id"]
+
+        self.assertEqual(serialized_query, arguments.VALID_CREATED_QUERY)
+
     @tag("controllers.query.create_query_search_term_no_results")
     def test_create_query_search_term_no_results(self) -> None:
-        """Success Case: Create a `Query` record with a
-        search term that yielded no results."""
+        """Success Case: Create a `Query` record with a search term
+        that yielded no results."""
+
+        query = QueryController.create_query(
+            user=arguments.CREATE_QUERY_USER_EMAIL,
+            search_term=arguments.SEARCH_TERM_NO_RESULTS,
+        )
+
+        self.assertIsInstance(query, QueryModel)
+
+        serialized_query = QuerySerializer(query).data
+        del serialized_query["created"]
+        del serialized_query["id"]
+
+        self.assertEqual(serialized_query, arguments.VALID_NO_RESULTS_QUERY)
 
     @tag("controllers.query.create_query_user_dne")
     def test_create_query_user_dne(self) -> None:
         """Fail Case: Create a `Query` record with a `User`
         that does not exist."""
+
+        with pytest.raises(AnalyticsError):
+            QueryController.create_query(
+                user=arguments.CREATE_QUERY_USER_EMAIL_DNE,
+                search_term=arguments.CREATE_QUERY_SEARCH_TERM,
+            )
