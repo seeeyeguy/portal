1b6770f4dadff71d1943648974b78af414795aae
Luis Ruelas Lisboa <Luis.RuelasLisboa@L3Harris.com>
2025-01-16 15:52:43 -0500

    PMBIPO-125: Implemented DELETE Favorites controller pytests and added test fixtures.

--- api/src/preferences/controllers/Favorite/tests/mutations/delete/default/arguments/__init__.py
+++ api/src/preferences/controllers/Favorite/tests/mutations/delete/default/arguments/__init__.py
@@ -2,3 +2,12 @@
 Arguments to be shared for Favorite's delete
 controller pytests.
 """
+
+# `Favorite` id used for testing deletion.
+DELETE_FAVORITE_FAVORITE_ID: int = 1
+
+# `Favorite` DNE id used for testing failure case.
+DELETE_FAVORITE_FAVORITE_ID_DNE: int = 9999
+
+# Number of deleted `Favorite` rows.
+DELETE_FAVORITE_DELETED_ROWS: int = 1
