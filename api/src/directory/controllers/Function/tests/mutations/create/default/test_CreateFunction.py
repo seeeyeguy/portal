ed6cf09b90659a78abed7aa8342a5019d08827fd
Robbins, Zemar (US) - SAS <zemar.robbins@l3harris.com>
2025-01-16 14:50:09 -0500

    PMBIPO-142: Implemented POST directory function pytests.

--- api/src/directory/controllers/Function/tests/mutations/create/default/test_CreateFunction.py
+++ api/src/directory/controllers/Function/tests/mutations/create/default/test_CreateFunction.py
@@ -2,9 +2,17 @@
 Collection of pytests for Function's create controller.
 """
 
+# pylint: disable=wrong-import-order
+import pytest
 from typing import List
 
 from django.test import tag, TestCase
+
+from directory.controllers import Function
+from directory.controllers.Function.tests.mutations.create.default import arguments
+from directory.exceptions import DirectoryError
+from directory.models.Function import Function as FunctionModel
+from directory.models.Function.serializers import FunctionSerializer
 
 
 @tag(
@@ -26,6 +34,29 @@ class TestCreateFunction(TestCase):
     def test_create_function(self) -> None:
         """Success Case: Create a `Function` record."""
 
+        function = Function.create_function(
+            name=arguments.CREATE_FUNCTION_NAME,
+            description=arguments.CREATE_FUNCTION_DESCRIPTION,
+        )
+
+        self.assertIsInstance(function, FunctionModel)
+
+        # Serialize `Function`.
+        serialized_function = FunctionSerializer(function).data
+
+        # Remove dynamic primary key and datetime fields before comparison.
+        del serialized_function["created"]
+        del serialized_function["id"]
+        del serialized_function["modified"]
+
+        self.assertEqual(serialized_function, arguments.CREATE_FUNCTION_EXPECTED_VALUES)
+
     @tag("controllers.function.create_function_duplicate_name")
     def test_create_function_duplicate_name(self) -> None:
         """Fail Case: Create a `Function` record with a duplicate name."""
+
+        with pytest.raises(DirectoryError):
+            _ = Function.create_function(
+                name=arguments.CREATE_FUNCTION_DUPLICATE_NAME,
+                description=arguments.CREATE_FUNCTION_DESCRIPTION,
+            )
