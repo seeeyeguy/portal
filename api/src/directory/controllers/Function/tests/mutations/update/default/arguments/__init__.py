7efbbbd444493c92a0b766666bf8ba72d06e0bf7
Cantrell Picou <Cantrell.Picou@l3harris.com>
2025-01-23 15:13:25 -0500

    PMBIPO-143 PUT Function Test and Documentation.

--- api/src/directory/controllers/Function/tests/mutations/update/default/arguments/__init__.py
+++ api/src/directory/controllers/Function/tests/mutations/update/default/arguments/__init__.py
@@ -2,3 +2,29 @@
 Arguments to be shared for Function's update
 controller pytests.
 """
+
+# User for updating the `Function` record.
+UPDATE_FUNCTION_USER_EMAIL: str = "May.Parker@harris.com"
+
+# Arguments for successfully updating `Function` record.
+UPDATE_FUNCTION_ID: int = 1
+UPDATE_FUNCTION_NAME: str = "Update Function"
+UPDATE_FUNCTION_DESCRIPTION: str = "Update Function Level 1."
+
+# Duplicate `Function` name for testing failure case.
+UPDATE_FUNCTION_DUPLICATE_NAME: str = "Human Resources"
+
+# Duplicate `Function` id for testing failure case.
+UPDATE_FUNCTION_ID_DUPLICATE: int = 2
+
+# Invalid `Function` id for testing failure case.
+UPDATE_FUNCTION_ID_DNE: int = 9999
+
+# Expected values for updated `Function`.
+UPDATE_FUNCTION_EXPECTED_VALUES: dict = {
+    "id": 1,
+    "name": "Update Function",
+    "description": "Update Function Level 1.",
+    "created": "2024-08-27T12:00:00-04:00",
+    "modified": "2024-08-27T12:00:00-04:00",
+}
--- api/src/directory/controllers/Function/tests/mutations/update/default/test_UpdateFunction.py
+++ api/src/directory/controllers/Function/tests/mutations/update/default/test_UpdateFunction.py
@@ -1,10 +1,16 @@
 """
 Collection of pytests for Function's update controller.
 """
-
+import pytest
 from typing import List
 
 from django.test import tag, TestCase
+
+from directory.controllers import Function
+from directory.controllers.Function.tests.mutations.update.default import arguments
+from directory.exceptions import DirectoryError
+from directory.models.Function import Function as FunctionModel
+from directory.models.Function.serializers import FunctionSerializer
 
 
 @tag(
@@ -26,10 +32,38 @@ class TestUpdateFunction(TestCase):
     def test_update_function(self) -> None:
         """Success Case: Update a `Function` record."""
 
+        # Update `Function` record.
+        function_record, _ = Function.update_function(
+            function_id=arguments.UPDATE_FUNCTION_ID,
+            name=arguments.UPDATE_FUNCTION_NAME,
+            description=arguments.UPDATE_FUNCTION_DESCRIPTION,
+        )
+
+        self.assertIsInstance(function_record, FunctionModel)
+
+        # Serialize `Function`.
+        serialized_function = FunctionSerializer(function_record).data
+
+        self.assertEqual(serialized_function, arguments.UPDATE_FUNCTION_EXPECTED_VALUES)
+
     @tag("controllers.function.update_function_record_dne")
     def test_update_function_record_dne(self) -> None:
         """Fail Case: Update a `Function` that does not exist."""
 
+        with pytest.raises(DirectoryError):
+            _ = Function.update_function(
+                function_id=arguments.UPDATE_FUNCTION_ID_DNE,
+                name=arguments.UPDATE_FUNCTION_NAME,
+                description=arguments.UPDATE_FUNCTION_DESCRIPTION,
+            )
+
     @tag("controllers.function.update_function_duplicate_name")
     def test_update_function_duplicate_name(self) -> None:
         """Fail Case: Update a `Function` record with a duplicate name."""
+
+        with pytest.raises(DirectoryError):
+            _ = Function.update_function(
+                function_id=arguments.UPDATE_FUNCTION_ID_DUPLICATE,
+                name=arguments.UPDATE_FUNCTION_DUPLICATE_NAME,
+                description=arguments.UPDATE_FUNCTION_DESCRIPTION,
+            )
