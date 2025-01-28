b05f258ad9b782678c6b156429724e8bf39dde09
Luis Ruelas Lisboa <Luis.RuelasLisboa@L3Harris.com>
2025-01-28 14:57:50 -0500

    PMBIPO-146: Implemented PUT SubFunction controller pytests.

--- api/src/directory/controllers/SubFunction/tests/mutations/update/default/test_UpdateSubFunction.py
+++ api/src/directory/controllers/SubFunction/tests/mutations/update/default/test_UpdateSubFunction.py
@@ -2,9 +2,16 @@
 Collection of pytests for SubFunction's update controller.
 """
 
+import pytest
 from typing import List
 
 from django.test import tag, TestCase
+
+from directory.controllers import SubFunction
+from directory.controllers.SubFunction.tests.mutations.update.default import arguments
+from directory.exceptions import DirectoryError
+from directory.models import SubFunction as SubFunctionModel
+from directory.models.SubFunction.serializers import SubFunctionSerializer
 
 
 @tag(
@@ -27,15 +34,57 @@ class TestUpdateSubFunction(TestCase):
     def test_update_subfunction(self) -> None:
         """Success Case: Update a `SubFunction` record."""
 
+        subfunction, rows_affected = SubFunction.update_subfunction(
+            subfunction_id=arguments.UPDATE_SUBFUNCTION_ID,
+            name=arguments.UPDATE_SUBFUNCTION_NAME,
+            description=arguments.UPDATE_SUBFUNCTION_DESCRIPTION,
+            function=arguments.UPDATE_SUBFUNCTION_FUNCTION_ID,
+        )
+
+        self.assertIsInstance(subfunction, SubFunctionModel)
+
+        self.assertEqual(
+            rows_affected, arguments.UPDATE_SUBFUNCTION_EXPECTED_ROWS_AFFECTED
+        )
+
+        # Serialize `SubFunction` instance.
+        serialized_subfunction: dict = SubFunctionSerializer(subfunction).data
+
+        self.assertDictEqual(serialized_subfunction, arguments.VALID_SUBFUNCTION)
+
     @tag("controllers.subfunction.update_subfunction_record_dne")
     def test_update_subfunction_record_dne(self) -> None:
         """Fail Case: Update a `SubFunction` that does not exist."""
+
+        with pytest.raises(DirectoryError):
+            _ = SubFunction.update_subfunction(
+                subfunction_id=arguments.UPDATE_SUBFUNCTION_ID_DNE,
+                name=arguments.UPDATE_SUBFUNCTION_NAME,
+                description=arguments.UPDATE_SUBFUNCTION_DESCRIPTION,
+                function=arguments.UPDATE_SUBFUNCTION_FUNCTION_ID,
+            )
 
     @tag("controllers.subfunction.update_subfunction_duplicate_name")
     def test_update_subfunction_duplicate_name(self) -> None:
         """Fail Case: Update a `SubFunction` record with a duplicate name."""
 
+        with pytest.raises(DirectoryError):
+            _ = SubFunction.update_subfunction(
+                subfunction_id=arguments.UPDATE_SUBFUNCTION_ID,
+                name=arguments.UPDATE_SUBFUNCTION_DUPLICATE_NAME,
+                description=arguments.UPDATE_SUBFUNCTION_DESCRIPTION,
+                function=arguments.UPDATE_SUBFUNCTION_FUNCTION_ID,
+            )
+
     @tag("controllers.subfunction.update_subfunction_function_dne")
     def test_update_subfunction_function_dne(self) -> None:
         """Fail Case: Update a `SubFunction` record with a given
         function id where that `Function` does not exist."""
+
+        with pytest.raises(DirectoryError):
+            _ = SubFunction.update_subfunction(
+                subfunction_id=arguments.UPDATE_SUBFUNCTION_ID,
+                name=arguments.UPDATE_SUBFUNCTION_NAME,
+                description=arguments.UPDATE_SUBFUNCTION_DESCRIPTION,
+                function=arguments.UPDATE_SUBFUNCTION_FUNCTION_ID_DNE,
+            )
