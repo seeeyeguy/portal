b2e1e194e3316e68ef1c24c716ec1c658f5db72d
Shemar Charles <shemar.charles@l3harris.com>
2025-01-29 10:39:20 -0500

    PMBIPO-150 PR 2: Implemented DELETE Tag controller pytest

--- api/src/directory/controllers/Tag/tests/mutations/delete/default/arguments/__init__.py
+++ api/src/directory/controllers/Tag/tests/mutations/delete/default/arguments/__init__.py
@@ -2,3 +2,17 @@
 Arguments to be shared for Tag's delete
 controller pytests.
 """
+
+# Id used for testing delete by Tag id tests.
+DELETE_TAG_BY_ID: int = 1
+
+# `User` email used for testing delete `Tag` record.
+DELETE_TAG_USER_EMAIL: str = "May.Parker@harris.com"
+
+# Valid `Tag` record dictionary.
+VALID_TAG_RECORDS: dict = {
+    "id": 1,
+    "label": "filter::site:Melbourne",
+    "created": "2024-08-27T12:00:00-04:00",
+    "modified": "2024-08-27T12:00:00-04:00",
+}
