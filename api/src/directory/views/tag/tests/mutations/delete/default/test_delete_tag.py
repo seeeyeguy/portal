8493a5aeb6aa9db67a4a0e2e03ae7c5c9d35984e
Shemar Charles <shemar.charles@l3harris.com>
2025-01-30 14:31:58 -0500

    PMBIPO-150 PR 3: Implemented DELETE Tag view tests and documentation

--- api/src/directory/views/tag/tests/mutations/delete/default/test_delete_tag.py
+++ api/src/directory/views/tag/tests/mutations/delete/default/test_delete_tag.py
@@ -4,8 +4,12 @@ Collection of pytests for Tag's delete view endpoint.
 
 from typing import List
 
+from django.contrib.auth.models import User
 from django.test import tag, TestCase
 from django.urls import reverse
+from rest_framework import status
+
+from directory.controllers.Tag.tests.mutations.delete.default import arguments
 
 
 @tag(
@@ -21,8 +25,15 @@ class TestDeleteTag(TestCase):
     Tests for DELETE /v1/directory/tags endpoint.
     """
 
+    def setUp(self) -> None:
+
+        super().setUp()
+        user = User.objects.get(email__iexact=arguments.DELETE_TAG_USER_EMAIL)
+        self.client.force_login(user=user)
+
     fixtures: List[str] = [
         "portal/models/fixtures/tags/tags.json",
+        "portal/models/fixtures/users/users.json",
     ]
 
     url: str = reverse("directory.tag")
@@ -30,3 +41,11 @@ class TestDeleteTag(TestCase):
     @tag("views.tag.delete_tag")
     def test_delete_tag(self) -> None:
         """Success Case: Delete a `Tag` record."""
+
+        request_url: str = f"{self.url}?id={arguments.DELETE_TAG_BY_ID}"
+        response = self.client.delete(request_url)
+
+        rows_affected = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(rows_affected, 1)
