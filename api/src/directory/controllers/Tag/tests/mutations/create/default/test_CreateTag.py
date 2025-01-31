0fb0b6a128206bb8106832741a40c4a745002758
Robbins, Zemar (US) - SAS <zemar.robbins@l3harris.com>
2025-01-31 10:20:06 -0500

    PMBIPO-148: Implemented POST directory tag pytests.

--- api/src/directory/controllers/Tag/tests/mutations/create/default/test_CreateTag.py
+++ api/src/directory/controllers/Tag/tests/mutations/create/default/test_CreateTag.py
@@ -2,9 +2,16 @@
 Collection of pytests for Tag's create controller.
 """
 
+import pytest
 from typing import List
 
 from django.test import tag, TestCase
+
+from directory.controllers import Tag
+from directory.controllers.Tag.tests.mutations.create.default import arguments
+from directory.exceptions import DirectoryError
+from directory.models import Tag as TagModel
+from directory.models.Tag.serializers import TagSerializer
 
 
 @tag(
@@ -26,10 +33,22 @@ class TestCreateTag(TestCase):
     def test_create_tag(self) -> None:
         """Success Case: Create a `Tag` record."""
 
+        tag_record = Tag.create_tag(label=arguments.CREATE_TAG_LABEL)
+
+        self.assertIsInstance(tag_record, TagModel)
+
+        # Serialize `Tag`.
+        serialized_tag = TagSerializer(tag_record).data
+
+        del serialized_tag["created"]
+        del serialized_tag["id"]
+        del serialized_tag["modified"]
+
+        self.assertEqual(serialized_tag, arguments.CREATE_TAG_EXPECTED_VALUE)
+
     @tag("controllers.tag.create_tag_duplicate_label")
     def test_create_tag_duplicate_label(self) -> None:
         """Fail Case: Create a `Tag` record with a duplicate label."""
 
-    @tag("controllers.tag.create_tag_empty_label")
-    def test_create_tag_empty_label(self) -> None:
-        """Fail Case: Create a `Tag` record with an empty label."""
+        with pytest.raises(DirectoryError):
+            _ = Tag.create_tag(label=arguments.CREATE_DUPLICATE_TAG)
