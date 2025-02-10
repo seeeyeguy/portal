14e7b360d59aced1056e2f2b50869b98be88e245
Michelle Smith <michelle.smith@l3harris.com>
2025-02-10 14:30:41 -0500

    PMBIPO-139 - Controller tests and fixtures for POST of employee-levels

--- api/src/directory/controllers/EmployeeLevel/tests/mutations/create/default/arguments/__init__.py
+++ api/src/directory/controllers/EmployeeLevel/tests/mutations/create/default/arguments/__init__.py
@@ -2,3 +2,20 @@
 Arguments to be shared for EmployeeLevel's create
 controller pytests.
 """
+
+# Arguments for successfully creating `EmployeeLevel` record.
+CREATE_EMPLOYEELEVEL_NAME_SUCCESSFUL = "Successful Employee Level"
+CREATE_EMPLOYEELEVEL_DESCRIPTION = "This is a test EmployeeLevel"
+CREATE_EMPLOYEELEVEL_LEVEL = 4
+
+# Arguments for duplicate values when creating `EmployeeLevel` record.
+CREATE_EMPLOYEELEVEL_NAME_DUPLICATE = "Executive"
+CREATE_EMPLOYEELEVEL_LEVEL_DUPLICATE = 3
+
+# Expected values for created `EmployeeLevel`.
+CREATE_EMPLOYEELEVEL_EXPECTED_VALUES: dict = {
+    "id": 4,
+    "name": "Successful Employee Level",
+    "description": "This is a test EmployeeLevel",
+    "level": 4,
+}
