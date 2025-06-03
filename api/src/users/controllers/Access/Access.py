d1263eb90db12acbc0732fd2b10e099d87e9983f
Luis Ruelas Lisboa <Luis.RuelasLisboa@L3Harris.com>
2025-06-03 11:44:44 -0400

    PMBIPO-292: Implemented POST Users Access endpoint controller and its pytests.

--- api/src/users/controllers/Access/Access.py
+++ api/src/users/controllers/Access/Access.py
@@ -12,12 +12,70 @@ and ultimately approved by a `User` with an appropriate
 `Access`.
 """
 
-from typing import List, Optional, Tuple
+import logging
+from typing import List, Optional, Set, Tuple
 
 from django.contrib.auth import models as AuthModels
 from django.db.models import QuerySet
+from django.utils import timezone
 
-from users import models
+from directory.models import SubFunction
+from request.models import Stage
+from users import exceptions, models
+
+LOGGER = logging.getLogger(__name__)
+
+TERMINAL_STAGE_LEVELS: Set[int] = {
+    Stage.StageLevels.REJECTED_BY_BUSINESS_PROCESS_EXPERT,
+    Stage.StageLevels.APPROVED_BY_SUPERUSER,
+    Stage.StageLevels.REJECTED_BY_SUPERUSER,
+}
+
+VALID_BUSINESS_PROCESS_EXPERT_STAGE_LEVELS: Set[int] = {Stage.StageLevels.SUBMITTED}
+
+VALID_SUPERUSER_STAGE_LEVELS: Set[int] = {
+    Stage.StageLevels.SUBMITTED,
+    Stage.StageLevels.APPROVED_BY_BUSINESS_PROCESS_EXPERT,
+}
+
+VALID_ROLE_STAGE_LEVELS_MAP: dict = {
+    models.Role.RoleLevels.DATA_STEWARD: {},
+    models.Role.RoleLevels.BUSINESS_PROCESS_EXPERT: VALID_BUSINESS_PROCESS_EXPERT_STAGE_LEVELS,
+    models.Role.RoleLevels.SUPERUSER: VALID_SUPERUSER_STAGE_LEVELS,
+}
+
+
+def _validate_stages_for_role(role_level: int, stages: QuerySet[Stage]) -> None:
+    """
+    Internal function that validates the `Stage`s provided
+    for the given `Role` level. If an invalid `Stage` is provided,
+    an exception is raised.
+
+    Accepts:
+        * role_level (int): Level of the `Role` used for the validation
+            against the `Stage`s.
+        * stages (QuerySet[Stage]): `Stage`s that will be validated.
+
+    Returns:
+        * None
+    """
+
+    # Filter out the valid `Stage`s for the given `Role` level to
+    # determine if any invalid `Stage`s are present.
+    invalid_stages_for_role: List[int] = list(
+        stages.exclude(level__in=VALID_ROLE_STAGE_LEVELS_MAP[role_level]).values_list(
+            "level", flat=True
+        )
+    )
+
+    # If `invalid_stages_for_role` is not empty, raise an exception.
+    if invalid_stages_for_role:
+        err_msg = (
+            f"Invalid Stages (levels={invalid_stages_for_role}) for "
+            f"Role (level={role_level}))."
+        )
+        LOGGER.error(err_msg)
+        raise exceptions.UsersError(err_msg, 400)
 
 
 class Access:
@@ -54,7 +112,106 @@ class Access:
             * access (models.Access): A new `Access` record for the given user.
         """
 
-        return models.Access()
+        try:
+
+            if not (admin and admin.is_authenticated):
+                err_msg = "Authentication required."
+                LOGGER.error(err_msg)
+                raise exceptions.UsersError(err_msg, 401)
+
+            # Verify the permissions of the `admin`.
+            if not models.Access.objects.filter(
+                user=admin,
+                role__level=models.Role.RoleLevels.SUPERUSER,
+                access_revoked_date__isnull=True,
+            ).exists():
+                err_msg = "Permissions Denied."
+                LOGGER.error(err_msg)
+                raise exceptions.UsersError(err_msg, 403)
+
+            # Fetch `User` record.
+            user_record: AuthModels.User = AuthModels.User.objects.get(
+                email__iexact=user
+            )
+
+            # Verify the `User` does not have already
+            # have an `Access` for the given `role_level`.
+            if user_record.accesses.filter(
+                role__level=role_level, access_revoked_date__isnull=True
+            ).exists():
+                err_msg = (
+                    f"Access for User(email={user}) & Role(level={role_level}) "
+                    "already exists."
+                )
+                LOGGER.error(err_msg)
+                raise exceptions.UsersError(err_msg, 400)
+
+            # Fetch `Role` record.
+            role: models.Role = models.Role.objects.get(level=role_level)
+
+            # If the `role` corresponds to a `Superuser` and subfunctions
+            #  were given, raise an exception.
+            if role.level == models.Role.RoleLevels.SUPERUSER and subfunctions:
+                err_msg = "Superusers do not need subfunctions."
+                LOGGER.error(err_msg)
+                raise exceptions.UsersError(err_msg, 400)
+
+            # Fetch `SubFunction` records.
+            subfunction_records: QuerySet[SubFunction] = SubFunction.objects.filter(
+                id__in=subfunctions
+            )
+            if subfunctions and subfunction_records.count() != len(subfunctions):
+                err_msg = f"Some SubFunctions (ids={subfunctions}) do not exist."
+                LOGGER.error(err_msg)
+                raise exceptions.UsersError(err_msg, 404)
+
+            # Fetch `Stage` records.
+            stage_records: QuerySet[Stage] = Stage.objects.filter(
+                level__in=stage_levels
+            )
+            if stage_levels and stage_records.count() != len(stage_levels):
+                err_msg = f"Some Stages (levels={stage_levels}) do not exist."
+                LOGGER.error(err_msg)
+                raise exceptions.UsersError(err_msg, 404)
+
+            # Verify `stage_records` does not contain a terminal `Stage`.
+            if stage_records.filter(level__in=TERMINAL_STAGE_LEVELS).exists():
+                err_msg = "Terminal Stages are not permitted."
+                LOGGER.error(err_msg)
+                raise exceptions.UsersError(err_msg, 400)
+
+            # Verify `stage_records` does not contain the `DRAFT` `Stage`.
+            if stage_records.filter(level=Stage.StageLevels.DRAFT).exists():
+                err_msg = "Draft Stage is not permitted."
+                LOGGER.error(err_msg)
+                raise exceptions.UsersError(err_msg, 400)
+
+            # Validate the `Stage`s for the `Role`.
+            _validate_stages_for_role(
+                role_level=role.level,
+                stages=stage_records,
+            )
+
+            # Create `Access` record.
+            access: models.Access = models.Access.objects.create(
+                user=user_record,
+                role=role,
+                access_granted_date=timezone.now(),
+            )
+
+            # Add `Stages` and `SubFunctions`.
+            access.stage.add(*stage_records)
+            access.subfunctions.add(*subfunction_records)
+
+            return access
+        except AuthModels.User.DoesNotExist as exc:
+            err_msg = f"User (email={user}) does not exist."
+            LOGGER.error(err_msg)
+            raise exceptions.UsersError(err_msg, 404) from exc
+        except models.Role.DoesNotExist as exc:
+            err_msg = f"Role (level={role_level}) does not exist."
+            LOGGER.error(err_msg)
+            raise exceptions.UsersError(err_msg, 404) from exc
 
     @staticmethod
     def revoke_access(access: int, admin: AuthModels.User) -> Tuple[models.Access, int]:
--- api/src/users/controllers/Access/tests/mutations/create/default/arguments/__init__.py
+++ api/src/users/controllers/Access/tests/mutations/create/default/arguments/__init__.py
@@ -2,3 +2,64 @@
 Arguments to be shared for Access's create
 controller pytests.
 """
+
+from typing import List
+
+CREATE_ACCESS_ADMIN_USER_EMAIL: str = "Tony.Stark@harris.com"
+
+CREATE_ACCESS_NON_ADMIN_USER_EMAIL: str = "Peter.Parker@harris.com"
+
+CREATE_ACCESS_USER_EMAIL: str = "Ben.Parker@harris.com"
+CREATE_ACCESS_ROLE_LEVEL: int = 2
+CREATE_ACCESS_SUBFUNCTION_IDS: List[int] = [3, 4]
+CREATE_ACCESS_STAGE_LEVELS: List[int] = [2]
+
+CREATE_ACCESS_ALREADY_EXISTS_USER_EMAIL: str = "May.Parker@harris.com"
+CREATE_ACCESS_ALREADY_EXISTS_ROLE_LEVEL: int = 3
+CREATE_ACCESS_ALREADY_EXISTS_SUBFUNCTION_IDS: List[int] = [1, 4]
+
+CREATE_ACCESS_USER_EMAIL_DNE: str = "DNE.User@harris.com"
+
+CREATE_ACCESS_ROLE_LEVEL_DNE: int = 99
+
+CREATE_ACCESS_SUBFUNCTION_IDS_DNE: List[int] = [888, 999]
+
+CREATE_ACCESS_STAGE_LEVELS_DNE: List[int] = [500, 700]
+
+CREATE_ACCESS_ROLE_NOT_PERMITTED_SUBFUNCTIONS_ROLE_LEVEL: int = 1
+CREATE_ACCESS_ROLE_NOT_PERMITTED_STAGES_ROLE_LEVEL: int = 3
+
+CREATE_ACCESS_TERMINAL_STAGES_NOT_PERMITTED_STAGE_LEVELS: List[int] = [100]
+
+CREATE_ACCESS_DRAFT_STAGE_NOT_PERMITTED_STAGE_LEVELS: List[int] = [1]
+
+CREATE_ACCESS_INVALID_STAGES_FOR_ROLE_STAGE_LEVELS: List[int] = [3]
+
+CREATE_ACCESS_EXPECTED_ACCESS: dict = {
+    "id": 4,
+    "user": {
+        "id": 4,
+        "username": "Ben.Parker@harris.com",
+        "email": "Ben.Parker@harris.com",
+        "first_name": "Ben",
+        "last_name": "Parker",
+        "is_active": True,
+    },
+    "role": {
+        "id": 2,
+        "created": "2024-08-27T12:00:00-04:00",
+        "name": "Business Process Expert",
+        "description": "Business Process Expert.",
+        "level": 2,
+    },
+    "access_revoked_date": None,
+    "stage": [
+        {
+            "id": 2,
+            "name": "Submitted",
+            "description": "A resource was submitted.",
+            "level": 2,
+        },
+    ],
+    "subfunctions": [4, 3],
+}
