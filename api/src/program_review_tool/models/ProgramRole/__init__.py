f43c7ab3367bda141655d57e2945cb856f2e3ffc
Robbins, Zemar (US) - SAS <zemar.robbins@l3harris.com>
2025-08-19 11:10:20 -0400

    PMBIPO-395:  Add model changes for program-review-tool .

---
+++ api/src/program_review_tool/models/ProgramRole/__init__.py
@@ -0,0 +1,10 @@
+"""
+`ProgramRole` module. `ProgramRole` represents the
+specific role that is assigned to a `ProgramMember` on
+a `Program`. A `ProgramRole`, at present, consists of either a
+Program Manager (PGM) or a Program Financial Analyst (PFA)
+which identifies the specific role a `ProgramMember` may have
+on a specific `Program`.
+"""
+
+from .ProgramRole import ProgramRole
