d62d1331814a11661da05852affece3f22f461b7
u215228 <nathaniel.charbonneau@l3harris.com>
2026-04-29 14:54:02 -0400

    PMXBISW-85: fixed initial controller code

--- api/src/program_review_tool/controllers/JobRun/JobRun.py
+++ api/src/program_review_tool/controllers/JobRun/JobRun.py
@@ -4,6 +4,7 @@ records within the `JobRun` table. `JobRun` capture information about jobs run.
 """
 
 import logging
+from typing import Optional
 from django.contrib.auth import models as AuthModels
 from datetime import datetime, timezone
 from program_review_tool.models.JobRun.JobRun import JobStatus
@@ -35,22 +36,45 @@ class JobRun:
             # Create the `JobRun` record
             job_run: models.JobRun = models.JobRun.objects.create(
                 job_name = job_name,
+                user = user_record,
                 status = JobStatus.RUNNING,
+                started_at = started_at,
             )
 
             return job_run
-        except:
-            err_msg = f"Job {job_name} failed"
             
-    # @staticmethod
-    # def complete_job_run(
-    #     job_run_id: int,
-    #     status: str,
-    #     finished_at: datetime,
-    #     error_msg: Optional[str] = None
-    # ):
-    #     return
-    #
+        except AuthModels.User.DoesNotExist as exc:
+            err_msg = f"User (username={user}) does not exist."
+            LOGGER.error(err_msg)
+            raise exceptions.ProgramReviewToolError(err_msg, 404) from exc
+
+    @staticmethod
+    def complete_job_run(
+        job_run_id: int,
+        status: str,
+        finished_at: Optional[datetime] = None,
+        error_msg: Optional[str] = None
+    ) -> models.JobRun:
+
+        # Fetch job_run
+        job_run: models.JobRun = models.JobRun.objects.get(
+            id = job_run_id
+        )
+        
+        duration = None
+        if finished_at:
+            duration = job_run.finished_at - job_run.started_at
+
+        # Update job_run fields
+        job_run.duration = duration
+        job_run.finished_at = finished_at
+        job_run.status = status
+        job_run.error_msg = error_msg
+
+        job_run.save()
+
+        return job_run
+
     # @staticmethod
     # def fetch_job_run():
     #     return
